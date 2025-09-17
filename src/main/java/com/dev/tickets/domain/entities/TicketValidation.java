d3536bf576017539864e54ce177fad093abd04c3
hungpb13 <hungpb13@gmail.com>
2025-09-17 17:42:50 +0700

    Creating Ticket Validation Entity

--- src/main/java/com/dev/tickets/domain/entities/TicketValidation.java
+++ src/main/java/com/dev/tickets/domain/entities/TicketValidation.java
@@ -1,24 +1,23 @@
 package com.dev.tickets.domain.entities;
 
-import com.dev.tickets.domain.enums.TicketStatusEnum;
+import com.dev.tickets.domain.enums.TicketValidationMethodEnum;
+import com.dev.tickets.domain.enums.TicketValidationStatusEnum;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "tickets")
+@Table(name = "ticket_validations")
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Ticket {
+public class TicketValidation {
 
     @Id
     @Column(name = "id", nullable = false, updatable = false)
@@ -27,20 +26,15 @@ public class Ticket {
 
     @Column(name = "status", nullable = false)
     @Enumerated(EnumType.STRING)
-    private TicketStatusEnum status;
+    private TicketValidationStatusEnum status;
+
+    @Column(name = "validation_method", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketValidationMethodEnum validationMethod;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ticket_type_id")
-    private TicketType ticketType;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "purchaser_id")
-    private User purchaser;
-
-    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
-    private List<TicketValidation> validations = new ArrayList<>();
-
-    // TODO: OR Code
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
 
     @CreatedDate
     @Column(name = "created_at", updatable = false, nullable = false)
